c6680faaef4c15a0d737eb57fff2450b2a37a4f1
MHendriF <hendrifebriansyah28@gmail.com>
2024-06-10 14:32:57 +0700

    add input, card, dialog, textarea, tiast

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,5 +1,5 @@
 import React from "react";
 
-export default function RootLayout({ children }: Readonly<{ children: ReactNode }>) {
+export default function RootLayout({ children }: Readonly<{ children: React.ReactNode }>) {
   return <main>{children}</main>;
 }
