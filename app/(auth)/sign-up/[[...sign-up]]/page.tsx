c653c5b082fa0a71d5f9603200a7762b21feaa9b
MHendriF <hendrifebriansyah28@gmail.com>
2024-06-10 14:00:22 +0700

    implement sign-in, sign-up using clerk

---
+++ app/(auth)/sign-up/[[...sign-up]]/page.tsx
@@ -0,0 +1,10 @@
+import { SignUp } from "@clerk/nextjs";
+import React from "react";
+
+export default function SignUpPage() {
+  return (
+    <main className="flex h-screen w-full items-center justify-center">
+      <SignUp />
+    </main>
+  );
+}
