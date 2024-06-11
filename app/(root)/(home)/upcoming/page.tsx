6d7ac91272760a6dc79250b5ae45500c0cb05214
MHendriF <hendrifebriansyah28@gmail.com>
2024-06-11 09:25:08 +0700

    implement upcoming meeting

--- app/(root)/(home)/upcoming/page.tsx
+++ app/(root)/(home)/upcoming/page.tsx
@@ -1,9 +1,11 @@
-import React from "react";
+import CallList from "@/components/CallList";
 
 export default function Upcoming() {
   return (
     <section className="flex size-full flex-col gap-10 text-white">
-      <h1 className="text-3xl font-bold">Upcoming</h1>
+      <h1 className="text-3xl font-bold">Upcoming Meeting</h1>
+
+      <CallList type="upcoming" />
     </section>
   );
 }
